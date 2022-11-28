f108d3a2f5093cd1f9847345dcd7ce2152b93e40
Efanova Elena <efanova-lena@yandex.ru>
2022-11-28 13:48:52 +0300

    Правки по замечаниям

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,22 +1,20 @@
 import {Component} from 'react';
 import { ingredients, selectedIngredients } from '../../utils/data';
-import styles from './App.module.css';
+import styles from './app.module.css';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 
-class App extends Component {
-  render() {
-    return (
-      <div className={'body'}>
-        <AppHeader />
-        <main className={styles.mainsection}>
-          <BurgerIngredients ingredients={ingredients} selectedIngredients={selectedIngredients}/>
-          <BurgerConstructor selectedIngredients={selectedIngredients}/>
-        </main>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className={'body'}>
+      <AppHeader />
+      <main className={styles.mainsection}>
+        <BurgerIngredients ingredients={ingredients} selectedIngredients={selectedIngredients} />
+        <BurgerConstructor selectedIngredients={selectedIngredients} />
+      </main>
+    </div>
+  )
 }
 
 export default App;
--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -1,20 +1,26 @@
 import { Component } from 'react';
+import { useState } from 'react';
 import { CurrencyIcon, Counter, Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import PropTypes from 'prop-types';
 import styles from './burger-ingredients.module.css';
+import { ingredientType } from '../../utils/types';
 
-class BurgerIngredients extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { activeTab: 'bun' };
-  }
+function BurgerIngredients(props) {
+  const [activeTab, setActiveTab] = useState('bun');
 
-  countIngredients = (ingredientId) => {
-    return this.props.selectedIngredients
+  const countIngredients = (ingredientId) => {
+    return props.selectedIngredients
       .filter(item => item['_id'] === ingredientId).length;
   }
 
-  renderIngredientCard = (ingredient) => {
+  const filterIngredientsByType = () => {
+    const buns = props.ingredients.filter(ingredient => ingredient.type === 'bun');
+    const sauces = props.ingredients.filter(ingredient => ingredient.type === 'sauce');
+    const mains = props.ingredients.filter(ingredient => ingredient.type === 'main');
+    return {buns, sauces, mains}
+  }
+
+  const renderIngredientCard = (ingredient) => {
     return (
       <li className={`${styles['ingredient-card']}`} key={ingredient['_id']}>
         <img
@@ -27,83 +33,50 @@ class BurgerIngredients extends Component {
           <CurrencyIcon type='primary' />
         </div>
         <h4 className={`${styles.name} text_type_main-default mt-1`}>{ingredient.name}</h4>
-        {this.countIngredients(ingredient['_id']) > 0 ?
-          <Counter count={this.countIngredients(ingredient['_id'])} size='default' /> :
+        {countIngredients(ingredient['_id']) > 0 ?
+          <Counter count={countIngredients(ingredient['_id'])} size='default' /> :
           null
         }
       </li>
     )
   }
 
-  render() {
-    const buns = this.props.ingredients.filter(ingredient => ingredient.type === 'bun');
-    const sauces = this.props.ingredients.filter(ingredient => ingredient.type === 'sauce');
-    const mains = this.props.ingredients.filter(ingredient => ingredient.type === 'main');
-    
-    return (
-      <section className={styles['burger-ingredients']}>
-        
-        <h1 className='text text_type_main-large mt-10 mb-5'>Соберите бургер</h1>
-        <div className={`${styles.tabs} mb-10`}>
-          <Tab value='bun' active={this.state.activeTab === 'bun'} onClick={() => this.setState({ activeTab: 'bun' })}>Булки</Tab>
-          <Tab value='sauce' active={this.state.activeTab === 'sauce'} onClick={() => this.setState({ activeTab: 'sauce' })}>Соусы</Tab>
-          <Tab value='main' active={this.state.activeTab === 'main'} onClick={() => this.setState({ activeTab: 'main' })}>Начинки</Tab>
-        </div>
+  return (
+    <section className={styles['burger-ingredients']}>
+      <h1 className='text text_type_main-large mt-10 mb-5'>Соберите бургер</h1>
+      <div className={`${styles.tabs} mb-10`}>
+        <Tab value='bun' active={activeTab === 'bun'} onClick={() => setActiveTab('bun')}>Булки</Tab>
+        <Tab value='sauce' active={activeTab === 'sauce'} onClick={() => setActiveTab('sauce')}>Соусы</Tab>
+        <Tab value='main' active={activeTab === 'main'} onClick={() => setActiveTab('main')}>Начинки</Tab>
+      </div>
 
-        <div className={`${styles.ingredients} custom-scroll`}>
-          <div className={`${styles['ingredient-type']}`}>
-            <h3 className={`${styles['type-name']} text text_type_main-medium `}>Булки</h3>
-            <ul className={`${styles['ingredient-cards']} mt-6 mb-1 ml-4 mr-4`}>
-              {buns.map(ingredient => this.renderIngredientCard(ingredient))}
-            </ul>
-          </div>
-          <div className={`${styles['ingredient-type']}`}>
-            <h3 className={`${styles['type-name']} text text_type_main-medium `}>Соусы</h3>
-            <ul className={`${styles['ingredient-cards']} mt-6 mb-1 ml-4 mr-4`}>
-              {sauces.map(ingredient => this.renderIngredientCard(ingredient))}
-            </ul>
-          </div>
-          <div className={`${styles['ingredient-type']}`}>
-            <h3 className={`${styles['type-name']} text text_type_main-medium `}>Начинки</h3>
-            <ul className={`${styles['ingredient-cards']} mt-6 mb-1 ml-4 mr-4`}>
-              {mains.map(ingredient => this.renderIngredientCard(ingredient))}
-            </ul>
-          </div>
+      <div className={`${styles.ingredients} custom-scroll`}>
+        <div className={`${styles['ingredient-type']}`}>
+          <h3 className={`${styles['type-name']} text text_type_main-medium `}>Булки</h3>
+          <ul className={`${styles['ingredient-cards']} mt-6 mb-1 ml-4 mr-4`}>
+            {filterIngredientsByType().buns.map(ingredient => renderIngredientCard(ingredient))}
+          </ul>
         </div>
-      </section>
-    )
-  }
+        <div className={`${styles['ingredient-type']}`}>
+          <h3 className={`${styles['type-name']} text text_type_main-medium `}>Соусы</h3>
+          <ul className={`${styles['ingredient-cards']} mt-6 mb-1 ml-4 mr-4`}>
+            {filterIngredientsByType().sauces.map(ingredient => renderIngredientCard(ingredient))}
+          </ul>
+        </div>
+        <div className={`${styles['ingredient-type']}`}>
+          <h3 className={`${styles['type-name']} text text_type_main-medium `}>Начинки</h3>
+          <ul className={`${styles['ingredient-cards']} mt-6 mb-1 ml-4 mr-4`}>
+            {filterIngredientsByType().mains.map(ingredient => renderIngredientCard(ingredient))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 BurgerIngredients.propTypes = {
-  selectedIngredients: PropTypes.arrayOf(PropTypes.shape({
-    '_id': PropTypes.string,
-    'name': PropTypes.string,
-    'type': PropTypes.string,
-    'proteins': PropTypes.number,
-    'fat': PropTypes.number,
-    'carbohydrates': PropTypes.number,
-    'calories': PropTypes.number,
-    'price': PropTypes.number,
-    'image': PropTypes.string,
-    'image_mobile': PropTypes.string,
-    'image_large': PropTypes.string,
-    '__v': PropTypes.number
-  })),
-  ingredients: PropTypes.arrayOf(PropTypes.shape({
-    '_id': PropTypes.string,
-    'name': PropTypes.string,
-    'type': PropTypes.string,
-    'proteins': PropTypes.number,
-    'fat': PropTypes.number,
-    'carbohydrates': PropTypes.number,
-    'calories': PropTypes.number,
-    'price': PropTypes.number,
-    'image': PropTypes.string,
-    'image_mobile': PropTypes.string,
-    'image_large': PropTypes.string,
-    '__v': PropTypes.number
-  }))
+  selectedIngredients: PropTypes.arrayOf(ingredientType),
+  ingredients: PropTypes.arrayOf(ingredientType)
 }
 
 export default BurgerIngredients
